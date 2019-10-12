74ddebeae57ef8339c9eef6fe6270388ff933baa
yudafu <yudafu@Richole.local>
2019-10-12 11:43:31 +0800

    .

--- mail.js
+++ mail.js
@@ -1,26 +1,24 @@
 var nodemailer = require("nodemailer")
 var transporter = nodemailer.createTransport({
     host: "smtp.mxhichina.com",
-    port: 465, // SMTP 端口
-    secureConnection: true, // 使用 SSL
+    port: 465,
+    secureConnection: true,
     auth: {
         user: "support@getitfree.shop",
         pass: "Getitfree!"
     }
 })
 
-// NB! No need to recreate the transporter object. You can use
-// the same transporter object for all e-mails
-
-// setup e-mail data with unicode symbols
 var mailOptions = {
-    from: "support@getitfree.shop", // 发件地址
-    to: "richole.yu@qq.com", // 收件列表
-    subject: "Test", // 标题
-    html: "<b>Hello world ?</b>" // html 内容
+    from: "support@getitfree.shop",
+    to: "richole.yu@gmail.com",
+    subject: "Test",
+    html: `
+        <h2>We released some new products quickly and see it</h2>
+        <p>Click <a href="https://getitfree.shop">here</a> to get more information!</p>
+    `
 }
 
-// send mail with defined transport object
 transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
         return console.log(error)
